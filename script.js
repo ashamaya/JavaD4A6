4362025ab951beb4ca74cd8b0359d94476f71763
ashamaya <ashatamang143@gmail.com>
2021-06-15 17:52:49 +0545

    complete

---
+++ script.js
@@ -0,0 +1,10 @@
+var myObj ={
+    apple:"red",
+    ball:"round",
+    cat:"meow",
+    dog:"bone"
+}
+
+for(let property in myObj){
+    console.log(myObj.apple + '\n' + myObj.ball + '\n' + myObj.cat + '\n' + myObj.dog);
+}
